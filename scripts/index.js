b4c66329245756fbf85b13e830e12912c746bfba
onlinespeedometer <112972327+onlinespeedometer@users.noreply.github.com>
2023-08-20 20:05:27 +0530

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -11,8 +11,12 @@ if ("geolocation" in navigator) {
 }
 
 function startWatching() {
+  const options = {
+      enableHighAccuracy: true, // Request high-accuracy tracking
+    };
+
     if (!watchId) {
-        watchId = navigator.geolocation.watchPosition(updateSpeed);
+        watchId = navigator.geolocation.watchPosition(updateSpeed,handleGeoError,options);
     }
     
 }
@@ -27,14 +31,29 @@ function stopWatching() {
 
 function updateSpeed(position) {
     const speedElement = document.querySelector(".speed-digit");
-   alert(position.coords.speed);
+    console.log(position);
 
     // Calculate speed using the position's speed property (in meters per second)
     const speed = Math.round((position.coords.speed || 0) * 3.6); // Convert to km/h and round
 
     // Convert speed to miles per hour
-    const speedMilesPerHour = Math.round(speedMetersPerSecond * 2.23694); // 1 m/s = 2.23694 mph
+    // const speedMilesPerHour = Math.round(speedMetersPerSecond * 2.23694); // 1 m/s = 2.23694 mph
 
     speedElement.textContent = speed.toString(); // Display as a string without decimals
     // document.getElementById("unit").textContent="Km/hr";
+}
+function handleGeoError(error) {
+  switch (error.code) {
+    case error.PERMISSION_DENIED:
+      alert('Geolocation permission denied by user.');
+      break;
+    case error.POSITION_UNAVAILABLE:
+      alert('Location information is unavailable.');
+      break;
+    case error.TIMEOUT:
+      alert('The request to get location timed out.');
+      break;
+    default:
+      alert('An unknown error occurred.');
+  }
 }
